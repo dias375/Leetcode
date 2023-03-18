138022fd61fca7ae90ff1cc8bd85c24560c4dc6d
dias375 <dias375@gmail.com>
2023-03-18 17:14:59 -0400

    First Commit

---
+++ src/main/java/org/example/IsPalindrome.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class IsPalindrome {
+    public boolean isPalindrome(int x) {
+        if(x<0){return false;}
+        String digits = String.valueOf(x);
+        String reverseDigits = new StringBuilder(digits).reverse().toString();
+        if (digits.equals(reverseDigits)){return true;}
+     return false;
+    }
+}
---
+++ src/main/java/org/example/TwoSum.java
@@ -0,0 +1,18 @@
+package org.example;
+
+class TwoSum {
+    int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[]{};
+        for(int i = 0; i < nums.length - 1; i++){
+            for(int j = 0; j < nums.length; j++){
+                if(i != j){
+                    if(nums[i]+nums[j]==target) {
+                        answer = new int[]{i, j};
+                        return answer;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
