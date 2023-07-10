a139c3a56a6626527fdf5a1ef78972b40b14441d
dias375 <dias375@gmail.com>
2023-07-10 12:45:34 -0400

    2023-07-10

--- src/main/java/com/leetcode/ThreeSum.java
+++ src/main/java/com/leetcode/ThreeSum.java
@@ -1,39 +1,42 @@
 package com.leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> threeSum = new ArrayList<>();
-        return threeSum;
+        return zeroSumCombinations(nonRepeatingCombinations(nums));
     }
+
+    private List<List<Integer>> zeroSumCombinations(List<List<Integer>> list){
+        List<List<Integer>> ans = new ArrayList<>();
+        for(List<Integer> nums : list) if(zeroSum(nums)) ans.add(nums);
+        return ans;
+    }
+
+    private Boolean zeroSum(List<Integer> numbers){
+        return (numbers.get(0) + numbers.get(1) + numbers.get(2)) == 0;
+    }
+
+    private List<List<Integer>> nonRepeatingCombinations(int[] nums) {
+        List<List<Integer>> possibleCombinations = new ArrayList<>();
+        for (int indexOne = 0; indexOne < nums.length; indexOne++){
+            for (int indexTwo = 0; indexTwo < nums.length; indexTwo++){
+                for (int indexThree = 0; indexThree < nums.length; indexThree++){
+                    if(indexOne==indexTwo || indexTwo == indexThree || indexThree == indexOne) continue;
+                    List<Integer> combination = Arrays.asList(nums[indexOne], nums[indexTwo], nums[indexThree]);
+                    Collections.sort(combination);
+                    if(possibleCombinations.contains(combination)) continue;
+                    possibleCombinations.add(combination);
+                }
+            }
+        }
+        return possibleCombinations;
+    }
+
 }
 
-//        int firstIndex = 0;
-//        for(int firstNum : nums){
-//            int secondIndex = 0;
-//            for(int secondNum : nums){
-//                secondIndex++;
-//                int thirdIndex = 0;
-//                for(int thirdNum : nums){
-//
-//                    if(firstNum == secondNum || secondNum == thirdNum || thirdNum == firstNum){
-//                        thirdIndex++;
-//                        continue;
-//                    }
-//                    if(firstNum + secondNum + thirdNum == 0){
-//                        List<Integer> validAnswer = new ArrayList<>(3);
-//                        validAnswer.add(firstNum);
-//                        validAnswer.add(secondNum);
-//                        validAnswer.add(thirdNum);
-//                        threeSum.add(validAnswer);
-//                    }
-//
-//                    thirdIndex++;
-//                }
-//            }
-//            firstIndex++;
-//        }
 
 
