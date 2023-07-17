ea073b8fb20466b1f9c84197e2c2db571d806cab
dias375 <dias375@gmail.com>
2023-07-17 14:12:23 -0400

    2023-07-17

---
+++ src/main/java/com/leetcode/FloodFill.java
@@ -0,0 +1,36 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FloodFill {
+
+    List<int[]> checkedCoordinates = new ArrayList<>();
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int[] sourceCoordinates = {sr, sc};
+        for(int[] coordinate : checkedCoordinates){
+            if(Arrays.equals(coordinate, sourceCoordinates)) return image;
+        }
+        checkedCoordinates.add(sourceCoordinates);
+
+        int xLength = image.length;
+        int yLength = image[0].length;
+        int sourceColor = image[sr][sc];
+        image[sr][sc] = color;
+
+        List<int[]> possibleNeighbours = new ArrayList<>();
+        if(sc+1 < yLength) possibleNeighbours.add(new int[]{sr, sc+1});
+        if(sr+1 < xLength) possibleNeighbours.add(new int[]{sr+1, sc});
+        if(sc-1 >= 0) possibleNeighbours.add(new int[]{sr, sc-1});
+        if(sr-1 >= 0) possibleNeighbours.add(new int[]{sr-1, sc});
+        possibleNeighbours.removeIf(coordinates -> image[coordinates[0]][coordinates[1]] != sourceColor);
+
+        for(int[] coordinates : possibleNeighbours){
+                floodFill(image, coordinates[0], coordinates[1], color);
+        }
+        return image;
+    }
+}
+
--- src/main/java/com/leetcode/Main.java
+++ src/main/java/com/leetcode/Main.java
@@ -3,5 +3,15 @@ package com.leetcode;
 public class Main {
     public static void main(String[] args) {
 
+        var run  = new FloodFill();
+
+        int[][]image = {{1,1,1},{1,1,0},{1,0,1}};
+        int sr = 1;
+        int sc = 1;
+        int color = 2;
+
+
+        run.floodFill(image, sr, sc, color);
+
     }
 }
